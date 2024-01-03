8d41a0e792d153f09dc125fb56e619f62fa668a3
mikeeq <miotk.mikolaj@gmail.com>
2024-01-03 10:30:13 +0100

    Add CI pipeline v2

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ set -eu -o pipefail
 MAVEN_VERSION=3.9.6
 
 docker run -u 0 -t --rm \
+  -v $(pwd):/repo \
+  -w /repo \
   jenkins/jenkins:slim-jdk21 /bin/bash -c " \
     mkdir -p /usr/share/maven /usr/share/maven/ref \
     && curl -fsSL -o /tmp/apache-maven.tar.gz https://apache.osuosl.org/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz \
